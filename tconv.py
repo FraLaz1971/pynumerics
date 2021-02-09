9d78b3ee748bbda77c5298922abd596b1c02c21c
Francesco Lazzarotto <francesco.lazzarotto@inaf.it>
2021-02-09 14:25:57 +0100

    Add files via upload

---
+++ tconv.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+def splash():
+	print("***********************************************")
+	print("*** program to convert temperature          ***")
+	print("*** from celsius to fahrenheit degrees      ***")
+	print("***********************************************")
+
+def conversion():
+	c = input("what is the temperature in degrees celsius?")
+	print("you selected", c)
+	f = (9/5)*int(c) + 32
+	print(c, "degrees celsius = ", f ,"degrees fahrenheit.")
+
+
+def ui_loop():
+	while (1):
+		a = input("do you want do another conversion (y/n)?")
+		if (a == "Y" or a == "y"): 
+			conversion()
+		elif (a == "n" or a == "N"): 
+			bye()
+			break
+		else:
+			print("please type y or n")
+
+def bye():
+	print("goodbye.")
+	k = input("press enter to close the terminal")
+
+
+
+splash()
+conversion()
+ui_loop()
+
+#!/usr/bin/env python3
+
+def splash():
+	print("***********************************************")
+	print("*** program to convert temperature          ***")
+	print("*** from celsius to fahrenheit degrees      ***")
+	print("***********************************************")
+
+def conversion():
+	c = input("temperature in celsius degrees ?")
+	print("you selected", c)
+	f = (9/5)*int(c) + 32
+	print(c, "degrees celsius = ", f ,"degrees fahrenheit.")
+
+
+def ui_loop():
+	while (1):
+		a = input("do you want do another conversion (y/n)?")
+		if (a == "Y" or a == "y"): 
+			conversion()
+		elif (a == "n" or a == "N"): 
+			bye()
+			break
+		else:
+			print("please type y or n")
+
+def bye():
+	print("goodbye.")
+	k = input("press enter to close the terminal")
+
+
+splash()
+conversion()
+ui_loop()
+
---
+++ test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+if (0 == 0 and 1 == -1):
+	print("ciao")
+else:
+	print("buh!")
